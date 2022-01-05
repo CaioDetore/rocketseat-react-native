3be18bdd8c583473eb3b79bf00a6ee5e052152c1
Caio Detore <caio.detore@aswti.com.br>
2022-01-05 17:59:35 -0300

    styled-components-forms

--- gofinances/App.tsx
+++ gofinances/App.tsx
@@ -1,6 +1,32 @@
 import React from 'react'
+import AppLaoding from 'expo-app-loading'
+import { ThemeProvider } from 'styled-components'
+
+import {
+  useFonts, 
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme' 
+import { Register } from './src/screens/Register'
 import { Dashboard } from './src/screens/Dashboard'
 
 export default function App(){
-  return <Dashboard />
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if(!fontsLoaded){
+    return <AppLaoding />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Register />
+    </ThemeProvider>
+  )
 }
---
+++ gofinances/src/screens/Register/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { Input } from '../../components/Form/Input'
+import { Button } from '../../components/Form/Button'
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton'
+
+import { 
+    Container ,
+    Header,
+    Title,
+    Form,
+    Fields
+} from './styles'
+
+export function Register(){
+    return (
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+
+            <Form>
+                <Fields>
+                    <Input  
+                        placeholder="Nome"
+                    />
+                    <Input 
+                        placeholder="Preço"
+                    />
+                    
+                    <TransactionTypeButton 
+                        type="up"
+                        title="Income"
+                    />                    
+                    <TransactionTypeButton 
+                        type="up"
+                        title="Income"
+                    />
+                </Fields>
+
+                <Button title="Enviar" />
+            </Form>
+
+        </Container>
+    )
+}
