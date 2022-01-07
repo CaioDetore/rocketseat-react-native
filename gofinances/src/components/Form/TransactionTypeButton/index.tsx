ac680b6192fe21dfb1b981c64a1d82769f81f6c5
Caio Detore <caio.detore@aswti.com.br>
2022-01-07 18:00:52 -0300

    Gesture-Handler

--- gofinances/src/components/Form/TransactionTypeButton/index.tsx
+++ gofinances/src/components/Form/TransactionTypeButton/index.tsx
@@ -15,16 +15,26 @@ const icons = {
 interface Props extends TouchableOpacityProps {
     type: 'up' | 'down';
     title: string;
+    isActive: boolean;
 } 
 
 export function TransactionTypeButton({
     type,
     title,
+    isActive,
     ...rest
 }: Props){
     return (
-        <Container {...rest}>
-            <Icon name={icons[type]}/>
+        <Container 
+            {...rest} 
+            isActive={isActive}
+            type={type}
+        >
+                
+            <Icon 
+                name={icons[type]}
+                type={type}
+            />
             <Title>
                 {title}
             </Title>
